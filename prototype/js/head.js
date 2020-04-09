f631ecb8d45aba3920849a95fefdb26b6e423232
Matthew Ryall <matthew@mexico86.co.uk>
2020-04-09 12:01:50 +0100

    Create include file for prototype common head elements

---
+++ prototype/js/head.js
@@ -0,0 +1,20 @@
+document.write('  \
+  <meta charset="utf-8">    \
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    \
+    <meta name="description" content="">    \
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">    \
+    <meta name="generator" content="Jekyll v3.8.6">    \
+    \
+    <!-- Favicons -->    \
+    <link rel="apple-touch-icon" href="img/favicons/apple-touch-icon.png" sizes="180x180">    \
+    <link rel="icon" href="img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">    \
+    <link rel="icon" href="img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">    \
+    <link rel="manifest" href="img/favicons/manifest.json">    \
+    <link rel="mask-icon" href="img/favicons/safari-pinned-tab.svg" color="#563d7c">    \
+    <link rel="icon" href="img/favicons/favicon.ico">    \
+    <meta name="msapplication-config" content="img/favicons/browserconfig.xml">    \
+    <meta name="theme-color" content="#563d7c">    \
+    \
+    <!-- Stylesheet -->    \
+    <link href="css/main.css" rel="stylesheet">    \
+');
